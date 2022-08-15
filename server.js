ec86386047038a5332dfaecb8e797c19f6071bba
seun-beta <adegokeseunfunmi1999@gmail.com>
2022-08-15 17:02:21 +0100

    [UPD] Add enviroment variable
    Signed-off-by: seun-beta <adegokeseunfunmi1999@gmail.com>

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-  
+  const favFood = process.env.FAV_FOOD
+
   async function main() {
     while(true) {
       console.log('Containers rule!');
+      console.log(`My favourite food is ${favFood}`);
       await sleep(5000);
     }
   }
